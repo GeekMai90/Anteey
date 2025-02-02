2ec6c658721f6e83ad736f1afb7fb07a88a4216c
GeekMai90 <166203003+GeekMai90@users.noreply.github.com>
2025-02-02 21:06:23 +0800

    优化卡片盒卡片的样式效果

--- src/renderer/src/components/cardbox/CardboxNoteCard.vue
+++ src/renderer/src/components/cardbox/CardboxNoteCard.vue
@@ -262,27 +262,24 @@ const emit = defineEmits(['cardbox-update'])
   border: 1px solid var(--color-border);
   border-radius: 8px;
   padding: 10px 0px 10px 0;
-  // margin-bottom: 15px;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   position: relative;
-  width: 100%; // 使用 100% 宽度
+  width: 100%;
   height: var(--card-height);
-  overflow: hidden; // 防止内容溢出
+  overflow: hidden;
   box-shadow: var(--shadow-card);
   user-select: none;
-  // box-shadow:
-  //   0 1px 2px rgba(0, 0, 0, 0.05),
-  //   0 4px 8px rgba(0, 0, 0, 0.05),
-  //   0 8px 16px rgba(0, 0, 0, 0.05);
-  // transition: box-shadow 0.3s ease-in-out;
-  // &:hover {
-  //   box-shadow:
-  //     0 2px 4px rgba(0, 0, 0, 0.05),
-  //     0 6px 12px rgba(0, 0, 0, 0.05),
-  //     0 12px 24px rgba(0, 0, 0, 0.05);
-  // }
+  backdrop-filter: blur(10px);
+  transition: all 0.3s ease;
+
+  &:hover {
+    border: 1px solid rgba(var(--color-primary-rgb), 0.4);
+    box-shadow: 0 0 20px 1px rgba(var(--color-primary-rgb), 0.1);
+    transform: translateY(-2px);
+  }
+
   .note-header {
     display: flex;
     align-items: center;
